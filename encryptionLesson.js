4cbc38137abdb7e71a7a8a098eb5340703fbb10e
Garrett Smith <19smitgr@users.noreply.github.com>
2018-02-10 16:24:32 -0500

    Update encryptionLesson.js

--- encryptionLesson.js
+++ encryptionLesson.js
@@ -1,1 +1,25 @@
-// Put your code here
+var instrument = "piano";
+console.log(instrument[2].charCodeAt());
+
+function encrypt() {
+    var originalMessage = document.getElementById('encrypt').value;
+    var encryptedMessage = "";
+    var letterNumber = 0;
+
+    while(letterNumber < originalMessage.length) {
+        var letter = originalMessage[letterNumber];
+
+        var charCode = letter.charCodeAt();
+
+        charCode = charCode * 5;
+
+        var newLetter = String.fromCharCode(charCode);
+
+        encryptedMessage += newLetter;
+
+        letterNumber += 1;
+    }
+
+    console.log(encryptedMessage);
+    document.getElementById('decrypt').innerHTML = encryptedMessage;
+}
